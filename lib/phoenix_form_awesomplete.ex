ae9e3f8ddffaa90f9323f84781ec55a603440c16
Nico Hoogervorst <n.g.c.hoogervorst@gmail.com>
2016-11-11 04:55:36 +0100

    split function to be able to get generated script

--- lib/phoenix_form_awesomplete.ex
+++ lib/phoenix_form_awesomplete.ex
@@ -2,19 +2,18 @@ defmodule PhoenixFormAwesomplete do
   alias Phoenix.HTML
   alias Phoenix.HTML.Form
 
+  # @util & @lea allows you to use aliases or replacement libraries. The default names are a bit long.
+  # For example use: `var aw = Awesomplete, au = AwesompleteUtil;`. This could shorten the average page size.
   @util Application.get_env(:phoenix_form_awesomplete, :util) || "AwesompleteUtil"
+  @lea  Application.get_env(:phoenix_form_awesomplete, :awesomplete) || "Awesomplete"
 
-  defp script(script) do
+  def script_tag(script) do
     HTML.raw("<script>#{script}</script>")
-  end
-
-  defp chars_to_string(text) do
-    if is_list(text), do: :erlang.list_to_binary(text), else: text
   end
 
   def copy_to_id(source_form, source_field, data_field \\ nil, target_id) 
       when (is_nil(data_field) or is_binary(data_field)) and is_binary(target_id) do
-    script(copy_to_id_script(source_form, source_field, data_field, target_id))
+    script_tag(copy_to_id_script(source_form, source_field, data_field, target_id))
   end
 
   # target_id can also be function, so you can control the output.
@@ -26,13 +25,13 @@ defmodule PhoenixFormAwesomplete do
     # or it can be an inlined function or it can be a function-name with bind
     is_function = String.contains?(target_id, "{") || target_id =~ ~r/^\w[^\s]*$/
     target = if is_function, do: target_id, else: "'#{target_id}'"
-    @util <> ".startCopy('##{source_id}', '#{data_field}', #{target});"
+    "#{@util}.startCopy('##{source_id}', '#{data_field}', #{target});"
   end
 
   def copy_to_field(source_form, source_field, data_field \\ nil, target_form, target_field) 
       when is_nil(data_field) or is_binary(data_field) do
     target_id = Form.field_id(target_form, target_field)
-    script(copy_to_id_script(source_form, source_field, data_field, "##{target_id}"))
+    script_tag(copy_to_id_script(source_form, source_field, data_field, "##{target_id}"))
   end
 
   defp opts_to_string(opts) do
@@ -49,7 +48,30 @@ defmodule PhoenixFormAwesomplete do
     # This method generates javascript code for using Awesomplete(Util) in a friendly way.
   #
   def awesomplete(form, field, opts \\ [], awesomplete_opts) do
+    script = awesomplete_script(form, field, awesomplete_opts)
+    HTML.html_escape([Form.text_input(form, field, opts), script_tag(script)])
+  end
+
+  #
+    # This method generates javascript code for using Awesomplete(Util) in a friendly way.
+  #
+  def awesomplete_script_tag(form, field, awesomplete_opts) do
+    script = awesomplete_script(form, field, awesomplete_opts)
+    script_tag(script)
+  end
+
+  #
+    # This method generates javascript code for using Awesomplete(Util) in a friendly way.
+  #
+  def awesomplete_script(form, field, awesomplete_opts) do
     element_id = Form.field_id(form, field)
+    awesomplete_script(element_id, awesomplete_opts)
+  end
+
+  #
+    # This method generates javascript code for using Awesomplete(Util) in a friendly way.
+  #
+  def awesomplete_script(element_id, awesomplete_opts) do
     awesomplete_opts = Enum.to_list awesomplete_opts
 
     # 
@@ -66,6 +88,7 @@ defmodule PhoenixFormAwesomplete do
     {descr_search,      awesomplete_opts} = Keyword.pop(awesomplete_opts, :descrSearch, false) 
     {fld_name,          awesomplete_opts} = Keyword.pop(awesomplete_opts, :value)
     {filter_fun,        awesomplete_opts} = Keyword.pop(awesomplete_opts, :filter)
+    {item_fun,          awesomplete_opts} = Keyword.pop(awesomplete_opts, :item)
     {label_fld,         awesomplete_opts} = Keyword.pop(awesomplete_opts, :label)
     {loadall,           awesomplete_opts} = Keyword.pop(awesomplete_opts, :loadall, false)
     {limit,             awesomplete_opts} = Keyword.pop(awesomplete_opts, :limit)
@@ -75,12 +98,16 @@ defmodule PhoenixFormAwesomplete do
     {url,               awesomplete_opts} = Keyword.pop(awesomplete_opts, :url)
     {url_end,           awesomplete_opts} = Keyword.pop(awesomplete_opts, :urlEnd)
 
+    # generated js code uses @util & @lea but in the input we expect the standard Awesomplete & AwesompleteUtil names.
+    starts_with = filter_fun == "Awesomplete.FILTER_STARTSWITH" or filter_fun == "AwesompleteUtil.filterStartsWith"
+
     # 
       # Be permissive about all kind of parameter combinations, but these we really can't handle:
     # 
     if is_nil(fld_name)  and descr_fld != nil, do: raise ArgumentError, "'descr' without 'value' parameter."
     if is_nil(fld_name)  and label_fld != nil, do: raise ArgumentError, "'label' without 'value' parameter."
     if is_nil(descr_fld) and descr_search, do: raise ArgumentError, "Cannot search description texts without knowing the description field. Please supply descr parameter."
+    if starts_with and descr_search, do: raise ArgumentError, "Cannot search description texts with a #{filter_fun} filter."
     
     # 
       # Convert limit to integer
@@ -112,7 +139,7 @@ defmodule PhoenixFormAwesomplete do
     # 
       # Select the text that should be considered as the current input.
       # Will be used in the convertInput function and in the filter.
-      # For multiple items, only the last item is considered to be input for the suggestion list.
+      # For multiple items, only the last item is considered to be input for the suggestion list lookup.
     # 
     conv_input_str = 
       if is_nil(multiple_char) do
@@ -121,21 +148,35 @@ defmodule PhoenixFormAwesomplete do
         "input.replace(/[#{multiple_char}]\\s*$/, '').match(/[^#{multiple_char}]*$/)[0]"
       end
 
-    data_val = if is_nil(fld_name) or is_nil(descr_fld), do: "data", else: "data.value"
-    starts_with = filter_fun == "Awesomplete.FILTER_STARTSWITH" or filter_fun == @util <> ".filterStartsWith"
-    starts_with_filter_fun = if is_nil(descr_fld) and is_nil(label_fld) and maxItems !== 0 and filter_fun == "Awesomplete.FILTER_STARTSWITH", do: "Awesomplete.FILTER_STARTSWITH", else: @util <> ".filterStartsWith"
+    filter_str =
+      if is_nil(multiple_char) do
+        "input" 
+      else
+        "input.match(/[^#{multiple_char}]*#{filter_match_sep}$/)[0]"
+      end
+
+    data_val = if is_nil(fld_name) or (is_nil(descr_fld) and is_nil(label_fld)), do: "data", else: "data.value"
+    starts_with_filter_fun = if is_nil(descr_fld) and is_nil(label_fld) and maxItems !== 0 and filter_fun == "Awesomplete.FILTER_STARTSWITH", do: "#{@lea}.FILTER_STARTSWITH", else: "#{@util}.filterStartsWith"
 
     filter_opts = cond do
       is_nil(multiple_char) and is_nil(filter_fun) and data_val == "data" -> []
       is_nil(multiple_char) and is_nil(descr_fld) and is_nil(label_fld) and maxItems !== 0 and (is_nil(filter_fun) or filter_fun == "Awesomplete.FILTER_CONTAINS") -> []
-      is_nil(multiple_char) and (is_nil(filter_fun) or filter_fun == "Awesomplete.FILTER_CONTAINS" or filter_fun == @util <> ".filterContains") -> [filter: @util <> ".filterContains"]
+      is_nil(multiple_char) and (is_nil(filter_fun) or filter_fun == "Awesomplete.FILTER_CONTAINS" or filter_fun == "AwesompleteUtil.filterContains") -> [filter: "#{@util}.filterContains"]
       is_nil(multiple_char) and data_val == "data" -> [filter: filter_fun]
-      is_nil(multiple_char) and starts_with and Keyword.has_key?(awesomplete_opts, :item) -> [filter: starts_with_filter_fun]
-      is_nil(multiple_char) and starts_with -> [filter: starts_with_filter_fun , item: @util <> ".itemStartsWith"]
+      is_nil(multiple_char) and starts_with and is_nil(item_fun) -> [filter: starts_with_filter_fun , item: "#{@util}.itemStartsWith"]
+      is_nil(multiple_char) and starts_with -> [filter: starts_with_filter_fun]
       is_nil(multiple_char) -> [filter: "function(data, input) { return (#{filter_fun})(data.value, input); }"]
-      is_nil(filter_fun) -> [filter: "function(data, input) { return Awesomplete.FILTER_CONTAINS(#{data_val}, input.match(/[^#{multiple_char}]*#{filter_match_sep}$/)[0]); }"]
-      true -> [filter: "function(data, input) { return (#{filter_fun})(#{data_val}, #{conv_input_str}); }"]
+      starts_with -> [filter: "function(data, input) { return #{@lea}.FILTER_STARTSWITH(#{data_val}, #{filter_str}); }"]
+      is_nil(filter_fun) -> [filter: "function(data, input) { return #{@lea}.FILTER_CONTAINS(#{data_val}, #{filter_str}); }"]
+      true -> [filter: "function(data, input) { return (#{filter_fun})(#{data_val}, #{filter_str}); }"]
     end 
+
+    filter_opts = cond do
+      is_nil(item_fun) and is_nil(multiple_char) -> filter_opts
+      is_nil(item_fun) and starts_with ->  filter_opts ++ [item: "function(text, input) { return #{@util}.itemStartsWith(text, #{filter_str}); }"]
+      is_nil(item_fun) ->  filter_opts ++ [item: "function(text, input) { return #{@util}.itemContains(text, #{filter_str}); }"]
+      true -> filter_opts ++ [item: "function(text, input) { return (#{item_fun})(text, #{filter_str}); }"]
+    end
 
     conv_input_opts = cond do
       is_nil(multiple_char) and is_nil(conv_input_fun) -> [] 
@@ -162,7 +203,7 @@ defmodule PhoenixFormAwesomplete do
       is_nil(descr_fld) and is_nil(label_fld) -> "rec['#{fld_name}']"
       is_nil(descr_fld) -> "{ label:#{label_str}, value:rec['#{fld_name}'] }"
       !descr_search -> "{ label: #{label_str}+'<p>'+(rec['#{descr_fld}'] || ''), value: rec['#{fld_name}'] }"
-      true -> "{ label: #{label_str}+'<p>'+" <> @util <> ".mark(rec['#{descr_fld}'] || '', input, #{starts_with}), value: rec['#{fld_name}']+'|'+(rec['#{descr_fld}'] || '').replace('|', ' ') }"
+      true -> "{ label: #{label_str}+'<p>'+#{@util}.mark(rec['#{descr_fld}'] || '', input, #{starts_with}), value: rec['#{fld_name}']+'|'+(rec['#{descr_fld}'] || '').replace('|', ' ') }"
     end
 
     data_fun_str = if is_nil(data_fun), do: "function(rec, input) { return #{data_fun_result}; }", else: "function(rec, input) { return (#{data_fun})(#{data_fun_result}, input); }"
@@ -193,15 +234,14 @@ defmodule PhoenixFormAwesomplete do
     util_opts = util_opts ++ conv_input_opts
     util_opts_str = "{" <> opts_to_string(util_opts) <> "}" 
 
-    awe_script = "#{assign_text}" <> @util <> ".startAwesomplete('##{element_id}', #{util_opts_str}, #{awe_opts_str})"
+    awe_script = "#{assign_text}#{@util}.startAwesomplete('##{element_id}', #{util_opts_str}, #{awe_opts_str})"
 
     script = cond do
        combobox == false ->    "#{awe_script};"
-       assign   == false ->    @util <> ".startClick('##{combo_btn_id}', #{awe_script});"
-       true -> "#{awe_script};\n" <> @util <> ".startClick('##{combo_btn_id}', #{assign_var});"
+       assign   == false ->    "#{@util}.startClick('##{combo_btn_id}', #{awe_script});"
+       true -> "#{awe_script};\n#{@util}.startClick('##{combo_btn_id}', #{assign_var});"
     end
 
-    HTML.html_escape([Form.text_input(form, field, opts), script(script)])
-
+    script
   end
 end
