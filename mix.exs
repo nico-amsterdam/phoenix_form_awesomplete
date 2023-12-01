7c09d945b55394ca8150c01b53d9489eefd97f32
Nico <n.g.c.hoogervorst@gmail.com@users.noreply.github.com>
2023-12-01 15:18:09 +0100

    debounce option: time in milliseconds to wait for additional user input before doing the ajax call to retrieve suggestions. It limits the rate at which the json service is called per user session.

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule PhoenixFormAwesomplete.Mixfile do
   use Mix.Project
 
-  @version "0.2.0"
+  @version "0.2.1"
   @source_url "https://github.com/nico-amsterdam/phoenix_form_awesomplete"
 
   def project do
