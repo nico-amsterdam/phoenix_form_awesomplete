2269bc3e413962dc73b482c70307aa5950ef2ebe
Nico <n.g.c.hoogervorst@gmail.com@users.noreply.github.com>
2024-03-13 18:41:03 +0100

    Package javascript modules

--- config/config.exs
+++ config/config.exs
@@ -33,22 +33,22 @@ import Config
     config :esbuild, 
       version: "0.17.11",
       module: [
-        args: ~w(js/index.js --format=esm --sourcemap --bundle --target=es2016 --outfile=../priv/static/assets/awesomplete_bundle.mjs),
+        args: ~w(js/index.js --format=esm --sourcemap --bundle --target=es2016 --outfile=../priv/static/awesomplete_bundle.mjs),
         cd: Path.expand("../assets", __DIR__),
         env: %{"NODE_PATH" => Path.expand("../deps", __DIR__)}
       ],
       main: [
-        args: ~w(js/index.js --format=cjs --sourcemap --bundle --target=es2016 --outfile=../priv/static/assets/awesomplete_bundle.cjs),
+        args: ~w(js/index.js --format=cjs --sourcemap --bundle --target=es2016 --outfile=../priv/static/awesomplete_bundle.cjs),
         cd: Path.expand("../assets", __DIR__),
         env: %{"NODE_PATH" => Path.expand("../deps", __DIR__)}
       ],
       cdn: [
-        args: ~w(js/index.js --format=iife --sourcemap --global-name=AwesompleteBundle --bundle --target=es2016 --outfile=../priv/static/assets/awesomplete_bundle.js),
+        args: ~w(js/index.js --format=iife --sourcemap --global-name=AwesompleteBundle --bundle --target=es2016 --outfile=../priv/static/awesomplete_bundle.js),
         cd: Path.expand("../assets", __DIR__),
         env: %{"NODE_PATH" => Path.expand("../deps", __DIR__)}
       ],
       cdn_min: [
-        args: ~w(js/index.js --format=iife --sourcemap --global-name=AwesompleteBundle --bundle --target=es2016 --minify --outfile=../priv/static/assets/awesomplete_bundle.min.js),
+        args: ~w(js/index.js --format=iife --sourcemap --global-name=AwesompleteBundle --bundle --target=es2016 --minify --outfile=../priv/static/awesomplete_bundle.min.js),
         cd: Path.expand("../assets", __DIR__),
         env: %{"NODE_PATH" => Path.expand("../deps", __DIR__)}
       ]
